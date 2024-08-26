7efa6e027ed8d62df28dc75472c216ad171a8372
Manuel Aguiar <113345121+manuel-aguiar@users.noreply.github.com>
2024-08-26 08:06:06 +0000

    generic Commit

---
+++ Webserv_sketch/Http/IHttpMethodFactory.hpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   IHttpMethodFactory.hpp                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmaria-d <mmaria-d@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/26 07:51:29 by mmaria-d          #+#    #+#             */
+/*   Updated: 2024/08/26 07:59:53 by mmaria-d         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef IHTTPMETHODFACTORY_HPP
+
+# define IHTTPMETHODFACTORY_HPP
+
+class IHttpMethodFactory
+{
+
+};
+
+
+
+#endif
