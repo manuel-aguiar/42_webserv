0a7ffe898912a24877c91d2bc0852def35463388
Manuel Aguiar <113345121+manuel-aguiar@users.noreply.github.com>
2024-08-26 08:09:08 +0000

    generic Commit

--- Webserv_sketch/Http/HttpMethodFactory.hpp
+++ Webserv_sketch/Http/HttpMethodFactory.hpp
@@ -1,22 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   IHttpMethodFactory.hpp                             :+:      :+:    :+:   */
+/*   HttpMethodFactory.hpp                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mmaria-d <mmaria-d@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/26 07:51:29 by mmaria-d          #+#    #+#             */
-/*   Updated: 2024/08/26 07:59:53 by mmaria-d         ###   ########.fr       */
+/*   Updated: 2024/08/26 08:09:05 by mmaria-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef IHTTPMETHODFACTORY_HPP
+#ifndef HTTPMETHODFACTORY_HPP
 
-# define IHTTPMETHODFACTORY_HPP
+# define HTTPMETHODFACTORY_HPP
 
-class IHttpMethodFactory
+# include "GET/HttpMethodGET.hpp"
+# include "HEAD/HttpMethodHEAD.hpp"
+# include "POST/HttpMethodPOST.hpp"
+# include "DELETE/HttpMethodDELETE.hpp"
+
+class HttpMethodFactory
 {
-
+    
 };
 
 
