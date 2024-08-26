ee046988e912dfc40b131bc8cc80840f7f11f0d4
Manuel Aguiar <113345121+manuel-aguiar@users.noreply.github.com>
2024-08-26 08:49:23 +0000

    generic Commit

--- Webserv_sketch/Http/Methods/IHttpMethod.hpp
+++ Webserv_sketch/Http/Methods/IHttpMethod.hpp
@@ -6,7 +6,7 @@
 /*   By: mmaria-d <mmaria-d@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/26 08:01:23 by mmaria-d          #+#    #+#             */
-/*   Updated: 2024/08/26 08:02:04 by mmaria-d         ###   ########.fr       */
+/*   Updated: 2024/08/26 08:49:21 by mmaria-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,8 @@
 
 class IHttpMethod
 {
-
+    virtual ~IHttpMethod() {};
+    
 };
 
 
