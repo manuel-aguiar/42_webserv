2d52f374a0b044fe05af54345096c4b5acee79a9
Manuel Aguiar <113345121+manuel-aguiar@users.noreply.github.com>
2024-08-26 14:29:01 +0000

    generic Commit

--- Webserv_sketch/ThreadPool/IThreadTaskQueue.hpp
+++ Webserv_sketch/ThreadPool/IThreadTaskQueue.hpp
@@ -6,7 +6,7 @@
 /*   By: mmaria-d <mmaria-d@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/26 13:31:43 by mmaria-d          #+#    #+#             */
-/*   Updated: 2024/08/26 14:13:32 by mmaria-d         ###   ########.fr       */
+/*   Updated: 2024/08/26 14:23:41 by mmaria-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,12 @@
 
 class IThreadTaskQueue
 {
-    virtual ~IThreadTaskQueue();
-    virtual void                addTask(const IThreadTask* newTask) = 0;
-    virtual IThreadTask*        getTask() = 0;
-    virtual void                clear() = 0;
-    virtual void                waitForCompletion() = 0;
+    public:
+        virtual ~IThreadTaskQueue();
+        virtual void                addTask(const IThreadTask* newTask) = 0;
+        virtual IThreadTask*        getTask() = 0;
+        virtual void                clear() = 0;
+        virtual void                waitForCompletion() = 0;
 };
 
 
--- Webserv_sketch/ThreadPool/ThreadTaskQueue.cpp
+++ Webserv_sketch/ThreadPool/ThreadTaskQueue.cpp
@@ -6,17 +6,40 @@
 /*   By: mmaria-d <mmaria-d@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/26 14:06:33 by mmaria-d          #+#    #+#             */
-/*   Updated: 2024/08/26 14:18:56 by mmaria-d         ###   ########.fr       */
+/*   Updated: 2024/08/26 14:28:58 by mmaria-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ThreadTaskQueue.hpp"
 
+ThreadTaskQueue::ThreadTaskQueue()
+{
+    pthread_mutex_init(&_taskAccess, NULL);
+    pthread_cond_init(&_newTaskSignal, NULL);
+    pthread_cond_init(&_allTasksDone, NULL);
+}
 
+ThreadTaskQueue::~ThreadTaskQueue()
+{
+    pthread_mutex_destroy(&_taskAccess);
+    pthread_cond_destroy(&_newTaskSignal);
+    pthread_cond_destroy(&_allTasksDone);
+}
 
+ThreadTaskQueue::ThreadTaskQueue(const ThreadTaskQueue& copy)
+{
 
+}
 
+ThreadTaskQueue& ThreadTaskQueue::operator=(const ThreadTaskQueue& copy)
+{
+    
+}
 
+void    ThreadTaskQueue::addTask(const IThreadTask* newTask)
+{
+    
+}
 
 
 
