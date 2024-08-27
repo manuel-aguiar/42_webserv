db0a0abd85781e83a2856967b13cbf19bb4c678e
Manuel Aguiar <113345121+manuel-aguiar@users.noreply.github.com>
2024-08-27 09:41:25 +0000

    generic Commit

--- Webserv_sketch/ThreadPool/ThreadPool.cpp
+++ Webserv_sketch/ThreadPool/ThreadPool.cpp
@@ -6,7 +6,7 @@
 /*   By: mmaria-d <mmaria-d@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/27 09:30:00 by mmaria-d          #+#    #+#             */
-/*   Updated: 2024/08/27 09:31:23 by mmaria-d         ###   ########.fr       */
+/*   Updated: 2024/08/27 09:33:52 by mmaria-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,10 @@
 
 ThreadPool::ThreadPool(/* args */)
 {
+
 }
 
 ThreadPool::~ThreadPool()
 {
+    
 }
--- Webserv_sketch/ThreadPool/ThreadPool.hpp
+++ Webserv_sketch/ThreadPool/ThreadPool.hpp
@@ -6,7 +6,7 @@
 /*   By: mmaria-d <mmaria-d@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/27 09:30:02 by mmaria-d          #+#    #+#             */
-/*   Updated: 2024/08/27 09:31:18 by mmaria-d         ###   ########.fr       */
+/*   Updated: 2024/08/27 09:37:29 by mmaria-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,20 @@
 
 # define THREADPOOL_HPP
 
-class ThreadPool
+# include <vector>
+
+# include "IThreadPool.hpp"
+# include "ThreadTaskQueue.hpp"
+
+
+class ThreadPool : public IThreadPool
 {
     public:
         ThreadPool(/* args */);
         ~ThreadPool();
     private:
-        /* data */
+        ThreadTaskQueue     queue;
+        std::vector
 
 };
 
