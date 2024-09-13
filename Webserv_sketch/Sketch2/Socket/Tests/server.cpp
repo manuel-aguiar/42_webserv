fa663f7824dbc30587e2071d43b384af3aea43d2
Manuel Aguiar <manuelmnaguiar00@gmail.com>
2024-09-13 19:23:04 +0100

    generic Commit

--- Webserv_sketch/Sketch2/Socket/Tests/server.cpp
+++ Webserv_sketch/Sketch2/Socket/Tests/server.cpp
@@ -6,7 +6,7 @@
 /*   By: mmaria-d <mmaria-d@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/10 09:14:50 by mmaria-d          #+#    #+#             */
-/*   Updated: 2024/09/13 19:21:53 by mmaria-d         ###   ########.fr       */
+/*   Updated: 2024/09/13 19:23:00 by mmaria-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,7 @@ int main(void)
     try
     {
         std::vector<UniquePtr<ACommunicationSocket> >   connections;
-        //std::map<int, UniquePtr<ACommunicationSocket> >  connections;
+        
         IPv4Address                                     ipv4(0, 8080);
         ServerSocket                                    server(ipv4, SOCK_STREAM, IPPROTO_TCP);
         ClientSocket                                    client(ipv4, SOCK_STREAM, IPPROTO_TCP);
@@ -40,22 +40,11 @@ int main(void)
         server.listen();
         client.connect();
         
-        //UniquePtr<ACommunicationSocket> connection = server.accept();
-        //connections.push_back(new UniquePtr<ACommunicationSocket>(connection));
-//
-        //(*connections[0])->send();
-        
-        
         connections.push_back(server.accept());
         connections[0]->send();
-        
-        //UniquePtr<ACommunicationSocket> connection = server.accept();
-        //connections[connection.get()->getFd()] = (server.accept());
-        //connections[connection.get()->getFd()]->send();
 
         client.receive();
         
-        //delete connections[0];
         return (0);
     }
     catch(const std::exception& e)
