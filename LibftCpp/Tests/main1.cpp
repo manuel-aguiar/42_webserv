23e7de5127c9f728cfd59ce7fa7ea94570f91b98
Manuel Aguiar <manuelmnaguiar00@gmail.com>
2024-09-13 09:00:49 +0100

    generic Commit

--- LibftCpp/Tests/main1.cpp
+++ LibftCpp/Tests/main1.cpp
@@ -6,13 +6,18 @@
 /*   By: mmaria-d <mmaria-d@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/12 08:14:01 by mmaria-d          #+#    #+#             */
-/*   Updated: 2024/09/13 08:51:55 by mmaria-d         ###   ########.fr       */
+/*   Updated: 2024/09/13 09:00:45 by mmaria-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libftcpp.hpp"
 
 #include <iostream>
+
+/*
+    c++ -Wall -Wextra -Werror -std=c++98 main1.cpp -o main
+*/
+
 
 class Test
 {
