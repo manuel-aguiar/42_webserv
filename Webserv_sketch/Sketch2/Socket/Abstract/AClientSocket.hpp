afb179869be0bfdfa22970740ded957a1ad7304e
Manuel Aguiar <113345121+manuel-aguiar@users.noreply.github.com>
2024-09-16 10:22:12 +0000

    generic Commit

--- Webserv_sketch/Sketch2/Socket/Abstract/AClientSocket.hpp
+++ Webserv_sketch/Sketch2/Socket/Abstract/AClientSocket.hpp
@@ -6,7 +6,7 @@
 /*   By: mmaria-d <mmaria-d@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/11 09:02:14 by mmaria-d          #+#    #+#             */
-/*   Updated: 2024/09/16 10:18:22 by mmaria-d         ###   ########.fr       */
+/*   Updated: 2024/09/16 10:21:08 by mmaria-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,8 @@
 # define ACLIENTSOCKET_HPP
 
 # include "ASocket.hpp"
-# include "IClientHandle.hpp"
-# include "ICommunicationHandle.hpp"
+# include "IClientSocket.hpp"
+# include "ICommunicationSocket.hpp"
 
 template <
     typename SockAddr
