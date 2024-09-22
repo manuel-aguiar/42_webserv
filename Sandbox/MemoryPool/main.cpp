13022fbc5db84cb8437d0a338627ef1ee7aefbcd
Manuel Aguiar <manuelmnaguiar00@gmail.com>
2024-09-22 19:03:43 +0100

    generic Commit

--- Sandbox/MemoryPool/main.cpp
+++ Sandbox/MemoryPool/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mmaria-d <mmaria-d@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/22 09:41:38 by mmaria-d          #+#    #+#             */
-/*   Updated: 2024/09/22 18:48:57 by mmaria-d         ###   ########.fr       */
+/*   Updated: 2024/09/22 19:03:24 by mmaria-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 #include "Memory_Pool.hpp"
 
 #include <map>
+#include <set>
 #include <string>
 #include <vector>
 #include <list>
@@ -57,18 +58,25 @@ int main(void)
     typedef std::map<int, std::string, std::less<int>, SharedMemoryPool<std::pair<int, std::string> > >             PoolMap;
     typedef std::map<int, std::string, std::less<int>, SharedMemoryPool<std::pair<int, std::string> > >::iterator   PoolMap_iter;
 
+    
+
     PoolMap map1(comparator, pool2);
     PoolMap map2(comparator, pool2);
+    
 
     map1[1] = "map1 one";
     map1[2] = "map1 two";
 
     map2[1] = "map2 one";
     map2[2] = "map2 two";
+
+
     map1[3] = "map1 three";
     map1[4] = "map1 four";
     map2[3] = "map2 three";
     map2[4] = "map2 four";
+
+
 
     for(PoolMap_iter iter = map1.begin(); iter != map1.end(); ++iter)
         std::cout << iter->first << ": " << iter->second << std::endl;
@@ -77,8 +85,20 @@ int main(void)
         std::cout << iter->first << ": " << iter->second << std::endl;
 
 
-    SharedMemoryPool<std::string, 4096> pool3 = make_SharedPool<std::string, 4096>();
-    
+
+    SharedMemoryPool<std::string, 4096> pool4 = make_SharedPool<std::string, 4096>();
+    std::less<std::string> comparator4;
+
+    typedef std::set<std::string, std::less<std::string>, SharedMemoryPool<std::string> >  PoolSet;
+    typedef std::set<std::string, std::less<std::string>, SharedMemoryPool<std::string> >::iterator  PoolSet_iter;
+
+    PoolSet set1(comparator4, pool4);
+
+    set1.insert("set1 one");
+    set1.insert("set2 two");
+
+    for(PoolSet_iter iter = set1.begin(); iter != set1.end(); ++iter)
+        std::cout << *iter << std::endl;
 
     return (0);
 }
