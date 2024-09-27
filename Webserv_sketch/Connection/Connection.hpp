00747dbcb7078f031cb60a275098da42f0361880
Manuel Aguiar <manuelmnaguiar00@gmail.com>
2024-09-27 16:43:22 +0100

    update

--- Webserv_sketch/Connection/Connection.hpp
+++ Webserv_sketch/Connection/Connection.hpp
@@ -6,7 +6,7 @@
 /*   By: mmaria-d <mmaria-d@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/27 14:55:54 by mmaria-d          #+#    #+#             */
-/*   Updated: 2024/09/27 15:55:31 by mmaria-d         ###   ########.fr       */
+/*   Updated: 2024/09/27 16:15:15 by mmaria-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,27 @@
 # define CONNECTION_HPP
 
 # include "../Webserver_Definitions.h"
+# include "../ListeningSocket/ListeningSocket.hpp"
+# include "../MemoryPool/MemoryPool.h"
+
 
 class Connection
 {
     public:
+        
+        static Connection* create(ListeningSocket* listener);
+
         void    reset();
 
     private:
-        t_socket        _sockfd;
-        t_sockaddr*     _addr;
-        t_socklen       _addrlen;
+        t_socket            _sockfd;
+        t_sockaddr*         _addr;
+        t_socklen           _addrlen;
+
+        //will be spawned via static create.()
+        Connection();
+        ListeningSocket*    _listener;      //pointer cause it may be reused
+        Nginx_MemoryPool&   _pool;          //will have its own pool
 };
 
 
---
+++ Webserv_sketch/Connection/ConnectionPool.hpp
@@ -0,0 +1,72 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ConnectionPool.hpp                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmaria-d <mmaria-d@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/27 16:13:23 by mmaria-d          #+#    #+#             */
+/*   Updated: 2024/09/27 16:43:12 by mmaria-d         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# ifndef CONNECTIONPOOL_HPP
+
+# define CONNECTIONPOOL_HPP
+
+# include "Connection.hpp"
+# include "../Webserver_Definitions.h"
+# include "../MemoryPool/MemoryPool.h"
+# include "../ListeningSocket/ListeningSocket.hpp"
+
+# include <queue>
+# include <list>
+
+class ConnectionPool
+{
+    public: 
+        ConnectionPool() : _spareConnections(NULL)
+        {
+            
+        }
+        
+        Connection* getConnection(ListeningSocket* listener)
+        {
+            Connection*     connection;
+
+            if (_spareConnections.size())
+            {
+                connection = _spareConnections.front();
+                _spareConnections.pop_front();
+            }
+            else
+                connection = _pool.allocate();
+            return (connection);
+        }
+
+        void returnConnection(Connection* connection)
+        {
+            connection->reset();
+            _spareConnections.push_front(connection);
+        }
+
+
+
+
+    
+    private:
+
+
+        std::list<Connection*, MemoryPool_Dealloc<Connection*> >       _spareConnections;
+        MemoryPool_Dealloc<Connection>                                 _pool;
+
+        void destroyConnection(Connection* connection)
+        {
+            _pool.deallocate(connection);
+        }
+};
+
+
+
+
+# endif
