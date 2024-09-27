03f6ecb95877a9509d7d6bf59bae3bce7d5ff202
Manuel Aguiar <manuelmnaguiar00@gmail.com>
2024-09-27 12:20:29 +0100

    generic Commit

--- Webserv_sketch/MemoryPool/MemoryPool.h
+++ Webserv_sketch/MemoryPool/MemoryPool.h
@@ -6,7 +6,7 @@
 /*   By: mmaria-d <mmaria-d@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/25 12:20:05 by mmaria-d          #+#    #+#             */
-/*   Updated: 2024/09/25 12:21:35 by mmaria-d         ###   ########.fr       */
+/*   Updated: 2024/09/27 12:20:26 by mmaria-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 
 # include "Alloc/SharedMPool_Alloc.hpp"
 # include "Dealloc/SharedMPool_Dealloc.hpp"
-# include "DeallocAligned/SharedMPool_AlignDealloc.hpp"
+# include "AlignDealloc/SharedMPool_AlignDealloc.hpp"
+# include "Nginx_MemoryPool/Nginx_MemoryPool.hpp"
 
 #endif
