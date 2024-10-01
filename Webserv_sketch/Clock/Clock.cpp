7f3f2e93945173258aa2bb41ebbc02652c033562
Manuel Aguiar <113345121+manuel-aguiar@users.noreply.github.com>
2024-10-01 15:39:26 +0000

    update

--- Webserv_sketch/Clock/Clock.cpp
+++ Webserv_sketch/Clock/Clock.cpp
@@ -6,33 +6,9 @@
 /*   By: mmaria-d <mmaria-d@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/27 15:52:57 by mmaria-d          #+#    #+#             */
-/*   Updated: 2024/10/01 14:46:22 by mmaria-d         ###   ########.fr       */
+/*   Updated: 2024/10/01 15:27:23 by mmaria-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Clock.hpp"
 
-Clock::Clock() : _time (0)
-{
-    
-}
-
-Clock::~Clock()
-{
-
-}
-
-
-
-
-
-Clock::Clock(const Clock& copy)
-{
-    _time = copy._time;
-}
-
-Clock& Clock::operator=(const Clock& assign)
-{
-    _time = assign._time;
-    return (*this);
-}
--- Webserv_sketch/Clock/Clock.hpp
+++ Webserv_sketch/Clock/Clock.hpp
@@ -6,7 +6,7 @@
 /*   By: mmaria-d <mmaria-d@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/27 15:52:22 by mmaria-d          #+#    #+#             */
-/*   Updated: 2024/10/01 14:52:28 by mmaria-d         ###   ########.fr       */
+/*   Updated: 2024/10/01 15:39:19 by mmaria-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,42 @@
 
 # define CLOCK_HPP
 
+# include "../Logs/Logs.h"
 # include <ctime>
+# include <sys/time.h>
+# include <cerrno>
+# include <cstring> 
 
 class Clock
 {
     public:
-        Clock();
+        Clock(ILog* _logFile);
         ~Clock();
         Clock(const Clock& copy);
         Clock& operator=(const Clock& assign);
 
-
-
         void    update();
 
     private:
-        int     _time;
+        ILog*           _logFile;
+        struct timeval  _start;
+        struct timeval  _now;
+        time_t          _elapsed_ms;
+        char            _buffer[32];    
 };
 
+
+Clock::Clock(ILog* _logFile) : _logFile(_logFile), _elapsed_ms(0)
+{
+    if (gettimeofday(&_start, NULL) == -1)
+        _logFile->record("gettimeofday(): " + std::string(std::strerror(errno)));
+}
+
+void    Clock::update()
+{
+    if (gettimeofday(&_now, NULL) == -1)
+        _logFile->record("gettimeofday(): " + std::string(std::strerror(errno)));
+    _elapsed_ms = (_now.tv_sec - _start.tv_sec) * 1000 + (_now.tv_usec - _start.tv_usec) / 1000;
+}
 
 #endif
