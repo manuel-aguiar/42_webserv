3152b387efff7b5d924b6dd28d6a8dc1e879d426
Manuel Aguiar <113345121+manuel-aguiar@users.noreply.github.com>
2024-10-02 09:24:03 +0000

    update

--- Webserv_sketch/ListeningSocket/ListeningSocket.cpp
+++ Webserv_sketch/ListeningSocket/ListeningSocket.cpp
@@ -6,7 +6,7 @@
 /*   By: mmaria-d <mmaria-d@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/27 14:52:40 by mmaria-d          #+#    #+#             */
-/*   Updated: 2024/10/02 08:19:28 by mmaria-d         ###   ########.fr       */
+/*   Updated: 2024/10/02 09:24:01 by mmaria-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -136,6 +136,8 @@ void    ListeningSocket::close()
 }
 
 
+
+//private
 ListeningSocket::ListeningSocket() : 
     _globals(NULL),
     _connectionPool(*((ConnectionPool*)NULL))
