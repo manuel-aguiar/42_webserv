9d0bf9c9dc7e196e16194f9da58b11dea256b00b
Manuel Aguiar <manuelmnaguiar00@gmail.com>
2024-10-03 10:00:54 +0100

    update

--- Webserv_sketch/FileDescriptor/FileDescriptor.cpp
+++ Webserv_sketch/FileDescriptor/FileDescriptor.cpp
@@ -6,7 +6,7 @@
 /*   By: mmaria-d <mmaria-d@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/03 09:49:34 by mmaria-d          #+#    #+#             */
-/*   Updated: 2024/10/03 09:54:42 by mmaria-d         ###   ########.fr       */
+/*   Updated: 2024/10/03 10:00:51 by mmaria-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,34 @@
 # include "../Globals/Globals.hpp"
 
 
-int  FileDescriptor::setNonBlocking(int fd, Globals* globals)
+int  FileDescriptor::setNonBlocking(t_fd fd, Globals* globals)
 {
-
+    return (_addFlags(fd, O_NONBLOCK, globals));
 }
 
-int  FileDescriptor::setCloseOnExec(int fd, Globals* globals)
+int  FileDescriptor::setCloseOnExec(t_fd fd, Globals* globals)
 {
-
+    return (_addFlags(fd, O_CLOEXEC, globals));
 }
 
-int  FileDescriptor::setFdFlags(int fd, int flags, Globals* globals)
+int  FileDescriptor::setCloseOnExec_NonBlocking(t_fd fd, Globals* globals)
 {
-
+    return (_addFlags(fd, O_NONBLOCK | O_CLOEXEC, globals));
 }
 
-int  FileDescriptor::setCloseOnExec_NonBlocking(int fd, Globals* globals)
-{
 
+int    FileDescriptor::_addFlags(t_fd fd, const int flags, Globals* globals)
+{
+    int currentFlags = ::fcntl(fd, F_GETFL, 0);
+    if (currentFlags == -1)
+    {
+        globals->_logFile->record("fcntl(): " + std::string(std::strerror(errno)));
+        return (0);
+    }
+    if (::fcntl(fd, F_SETFL, currentFlags | flags) == -1)
+    {
+        globals->_logFile->record("fcntl(): " + std::string(std::strerror(errno)));
+        return (0);
+    }
+    return (1);
 }
--- Webserv_sketch/FileDescriptor/FileDescriptor.hpp
+++ Webserv_sketch/FileDescriptor/FileDescriptor.hpp
@@ -6,7 +6,7 @@
 /*   By: mmaria-d <mmaria-d@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/03 09:49:44 by mmaria-d          #+#    #+#             */
-/*   Updated: 2024/10/03 09:53:55 by mmaria-d         ###   ########.fr       */
+/*   Updated: 2024/10/03 09:58:35 by mmaria-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,19 @@
 # define FILEDESCRIPTOR_HPP
 
 # include <fcntl.h>
+# include "../Webserver_Definitions.h"
 
 class Globals;
 
 class FileDescriptor
 {
-    static int  setNonBlocking(int fd, Globals* globals);
-    static int  setCloseOnExec(int fd, Globals* globals);
-    static int  setFdFlags(int fd, int flags, Globals* globals);
-    static int  setCloseOnExec_NonBlocking(int fd, Globals* globals);
+    public:
+        static int  setNonBlocking                  (t_fd fd, Globals* globals);
+        static int  setCloseOnExec                  (t_fd fd, Globals* globals);
+        static int  setFdFlags                      (t_fd fd, const int flags, Globals* globals);
+        static int  setCloseOnExec_NonBlocking      (t_fd fd, Globals* globals);
+    private:
+        static int _addFlags                        (t_fd fd, const int flags, Globals* globals);
 };
 
 #endif
