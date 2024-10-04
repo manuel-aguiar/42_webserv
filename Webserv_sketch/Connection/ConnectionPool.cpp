050bb2606a33b06da8adfc71818ccbbd506c89e1
Manuel Aguiar <manuelmnaguiar00@gmail.com>
2024-10-04 10:37:33 +0100

    update

--- Webserv_sketch/Connection/ConnectionPool.cpp
+++ Webserv_sketch/Connection/ConnectionPool.cpp
@@ -6,7 +6,7 @@
 /*   By: mmaria-d <mmaria-d@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/27 16:17:47 by mmaria-d          #+#    #+#             */
-/*   Updated: 2024/10/03 17:01:38 by mmaria-d         ###   ########.fr       */
+/*   Updated: 2024/10/04 10:37:08 by mmaria-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,30 +16,32 @@
 ConnectionPool::~ConnectionPool() 
 {
     for (size_t i = 0; i < _connections.size(); i++)
-        _connections[i]._memPool->destroy();
+        _connections[i]->_memPool->destroy();
 }
 
 ConnectionPool::ConnectionPool(Globals* globals, size_t maxConnections) : 
     _globals(globals), 
     _maxConnections(maxConnections),
+    _connections(maxConnections),
+    _readEvents(maxConnections),
+    _writeEvents(maxConnections),
     _spareConnections(MPool_FixedElem<Connection*>(maxConnections))
 {
-    _connections.reserve(maxConnections);
-    _readEvents.reserve(maxConnections);
-    _writeEvents.reserve(maxConnections);
+
 
     for (size_t i = 0; i < maxConnections; i++)
     {
-        _connections.push_back(Connection(_globals));
-        _readEvents.push_back(Event(_globals));
-        _writeEvents.push_back(Event(_globals));
-        _connections[i].init();
-        _connections[i]._readEvent = &_readEvents[i];
-        _connections[i]._writeEvent = &_writeEvents[i];
-        _readEvents[i].setHandler_Function_and_Data(&HandlerFunction::connection_Read, &_connections[i]);
-        _readEvents[i].setFlags(EPOLLIN);
-        _writeEvents[i].setHandler_Function_and_Data(&HandlerFunction::connection_Write, &_connections[i]);
-        _writeEvents[i].setFlags(EPOLLOUT);
+        new (&_connections[i]) Connection(_globals);
+        new (&_readEvents[i]) Event(_globals);
+        new (&_writeEvents[i]) Event(_globals);
+        
+        _connections[i]->init();
+        _connections[i]->_readEvent = &_readEvents[i];
+        _connections[i]->_writeEvent = &_writeEvents[i];
+        _readEvents[i]->setHandler_Function_and_Data(&HandlerFunction::connection_Read, &_connections[i]);
+        _readEvents[i]->setFlags(EPOLLIN);
+        _writeEvents[i]->setHandler_Function_and_Data(&HandlerFunction::connection_Write, &_connections[i]);
+        _writeEvents[i]->setFlags(EPOLLOUT);
         _spareConnections.push_back(&_connections[i]);
     }
 }
