34344c3bf0fbd620d88ebe83ad4e2dd302a65615
Manuel Aguiar <manuelmnaguiar00@gmail.com>
2024-10-05 11:50:01 +0100

    update

--- Webserv_sketch/Connection/ConnectionPool.hpp
+++ Webserv_sketch/Connection/ConnectionPool.hpp
@@ -6,7 +6,7 @@
 /*   By: mmaria-d <mmaria-d@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/27 16:13:23 by mmaria-d          #+#    #+#             */
-/*   Updated: 2024/10/04 16:24:14 by mmaria-d         ###   ########.fr       */
+/*   Updated: 2024/10/05 09:43:08 by mmaria-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@
 # include <list>
 # include <vector>
 
-# define MAX_CONNECTIONS 1000
+# define MAX_CONNECTIONS 3
 
 class ConnectionPool
 {
