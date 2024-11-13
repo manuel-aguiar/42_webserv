ec190a05c5ff3be401a88da48bf37317d9e1443a
Manuel Aguiar <113345121+manuel-aguiar@users.noreply.github.com>
2024-11-13 12:27:05 +0000

    generic Commit

--- Webserv/Http/HttpRequest/HttpRequest.hpp
+++ Webserv/Http/HttpRequest/HttpRequest.hpp
@@ -6,7 +6,7 @@
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/11 10:17:04 by codespace         #+#    #+#             */
-/*   Updated: 2024/11/13 12:18:48 by codespace        ###   ########.fr       */
+/*   Updated: 2024/11/13 12:25:04 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,11 +23,14 @@ class HttpRequest
 	//setters forever
 
 	//variables to be filed by parsing the request
+	// ideas:    nginx-master/src/http/ngx_http_parse.c			//in reality they store pointers to where components start and end to avoid copying everything
+
 	std::string 						m_method;
 	std::string 						m_uri;
 	std::string 						m_httpVersion;
 	std::map<std::string, std::string> 	m_headers;
 
+		// URI components
 	std::string							m_host;
 	std::string							m_resource;
 	std::string							m_query;
