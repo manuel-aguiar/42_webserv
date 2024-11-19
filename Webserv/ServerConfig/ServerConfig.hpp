56bf9700226a5897a49e22487e30e4a0f8875b8f
Manuel Aguiar <113345121+manuel-aguiar@users.noreply.github.com>
2024-11-19 13:10:09 +0000

    reorganization in progress

---
+++ Webserv/ServerConfig/ServerConfig.hpp
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ServerConfig.hpp                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmaria-d <mmaria-d@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/19 12:50:49 by mmaria-d          #+#    #+#             */
+/*   Updated: 2024/11/19 12:57:37 by mmaria-d         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SERVERCONFIG_HPP
+
+# define SERVERCONFIG_HPP
+
+class DefaultConfig;
+class ServerBlocks;
+class ServerLocations;
+class Globals;
+
+class ServerConfig
+{
+    public:
+        ServerConfig(const char* configFile, const DefaultConfig& configDefault, Globals* globals);
+        
+
+    private:
+        ServerConfig();
+        
+
+};
+
+
+
+
+#endif
