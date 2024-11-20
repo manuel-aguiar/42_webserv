f6a6d64c6fe0507fddef6ef3f6c8223dd3d44d8d
Manuel Aguiar <manuelmnaguiar00@gmail.com>
2024-11-20 14:29:52 +0000

    ammended ServerBlocks, getListeners (pairs of IP + port)

--- Webserv/ServerConfig/ServerBlocks/ServerBlocks.hpp
+++ Webserv/ServerConfig/ServerBlocks/ServerBlocks.hpp
@@ -6,7 +6,7 @@
 /*   By: manuel <manuel@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/18 10:19:34 by mmaria-d          #+#    #+#             */
-/*   Updated: 2024/11/20 10:18:14 by manuel           ###   ########.fr       */
+/*   Updated: 2024/11/20 14:29:16 by manuel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@
 # include <string>
 # include <map>
 # include <set>
+# include <vector>
 
 class ServerLocations;
 
@@ -26,21 +27,20 @@ class ServerBlocks
     public:
         //constructor destructors
 
-        const std::set<std::string>&					getDomainName() const;
-		const std::string& 								getIp() const;
-		const std::string& 								getPort() const;
-		const std::string& 								getRoot() const;
-		const std::map<std::string, ServerLocations>& 	getLocations() const;
+		typedef std::pair<std::string, std::string>			t_listeners;
+
+
+        const std::set<std::string>&						getDomainNames() const;
+		const std::vector<t_listeners>&						getListeners() const;
+		const std::map<std::string, ServerLocations>& 		getLocations() const;
 
 
     private:
 
-
-        std::set<std::string>                   m_ServerName;		//would eb easier to only accept one name per serverblock
-		std::string							 	m_ip;
-        std::string                             m_port;
-        std::string                             m_root;
-        std::map<std::string, ServerLocations>   m_locations;		//std::string would be the path of the location
+        std::set<std::string>                   			m_ServerName;		//would eb easier to only accept one name per serverblock
+		std::vector<t_listeners>							m_listeners;
+        std::string                             			m_root;
+        std::map<std::string, ServerLocations>   			m_locations;		//std::string would be the path of the location
 
 };
 
