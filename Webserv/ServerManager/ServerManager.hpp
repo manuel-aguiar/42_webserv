888192516db8e52589e8e8c117bc4f17720dc520
Manuel Aguiar <manuelmnaguiar00@gmail.com>
2024-11-22 10:44:06 +0000

    reorganization in progress

--- Webserv/ServerManager/ServerManager.hpp
+++ Webserv/ServerManager/ServerManager.hpp
@@ -6,7 +6,7 @@
 /*   By: manuel <manuel@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/21 10:44:43 by mmaria-d          #+#    #+#             */
-/*   Updated: 2024/11/22 08:59:58 by manuel           ###   ########.fr       */
+/*   Updated: 2024/11/22 10:43:51 by manuel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 
 // Project Headers
 # include "ServerWorker/ServerWorker.hpp"
+# include "BlockFinder/BlockFinder.hpp"
 
 // C++ headers
 # include <vector>
@@ -23,6 +24,7 @@
 class ServerConfig;
 class Globals;
 class ThreadPool;
+class BlockFinder;
 
 class ServerManager
 {
@@ -37,7 +39,7 @@ class ServerManager
 		const ServerConfig&             m_config;
         ThreadPool*                     m_threadPool;
         Globals*                        m_globals;
-
+		BlockFinder						m_blockFinder;
 
         void    mf_runSingleThreaded();
         void    mf_runMultiThreaded();
