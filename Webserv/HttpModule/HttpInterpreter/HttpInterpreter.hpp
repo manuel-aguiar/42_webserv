bc1a3bd81137c345a344a9bb2320966c72849fd4
Manuel Aguiar <113345121+manuel-aguiar@users.noreply.github.com>
2024-11-26 16:01:42 +0000

    reorganization in progress

--- Webserv/HttpModule/HttpInterpreter/HttpInterpreter.hpp
+++ Webserv/HttpModule/HttpInterpreter/HttpInterpreter.hpp
@@ -6,7 +6,7 @@
 /*   By: mmaria-d <mmaria-d@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/11 10:16:50 by codespace         #+#    #+#             */
-/*   Updated: 2024/11/26 15:10:58 by mmaria-d         ###   ########.fr       */
+/*   Updated: 2024/11/26 15:24:02 by mmaria-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,48 @@
 
 # define HTTPINTERPRETER_HPP
 
+# include <map>
 
+class HttpSession;
 
 class HttpInterpreter
 {
+	public:
+		HttpInterpreter();
+		~HttpInterpreter();
+
+	private:
+
+		typedef std::string t_sessionID;
+
+		std::map<t_sessionID, HttpSession>	m_sessions;
 
 
-
+		HttpInterpreter(const HttpInterpreter& copy);
+		HttpInterpreter& operator=(const HttpInterpreter& assign);
 
 };
 
 
 
 #endif
+
+
+/*
+
+http
+{
+	server {
+		listen 80;
+	}
+}
+
+mail
+{
+	server {
+		listen 80;
+	}	
+}
+
+
+*/
