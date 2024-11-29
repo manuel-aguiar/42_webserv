42b1da1bbcabb31e840c464d4454413fd2ff98ad
Manuel Aguiar <manuelmnaguiar00@gmail.com>
2024-11-29 16:32:37 +0000

    reorganization in progress

--- Webserv/HttpModule/HttpModule.cpp
+++ Webserv/HttpModule/HttpModule.cpp
@@ -6,7 +6,7 @@
 /*   By: manuel <manuel@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/28 11:10:13 by manuel            #+#    #+#             */
-/*   Updated: 2024/11/29 09:51:07 by manuel           ###   ########.fr       */
+/*   Updated: 2024/11/29 16:29:22 by manuel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,9 @@
 # include "../Connection/Connection.hpp"
 # include "HttpConnection/HttpConnection.hpp"
 
-HttpModule::HttpModule(ServerManager& serverManager) : m_manager(serverManager)
+HttpModule::HttpModule(ServerManager& serverManager) :
+	m_serverManager(serverManager),
+	m_httpmanager(m_serverManager)
 {
 
 }
@@ -26,7 +28,7 @@ HttpModule::~HttpModule()
 
 void HttpModule::initConnection(Connection* accepted)
 {
-	HttpManager& 	manager = (reinterpret_cast<HttpModule*>(accepted->accessProtoModule()))->accessManager();
+	HttpManager& 	manager = (reinterpret_cast<HttpModule*>(accepted->accessProtoModule()))->accessHttpManager();
 
 	HttpConnection* conn = new HttpConnection(accepted);
 	accepted->setProtoConnection(conn);
