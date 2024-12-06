75c099b0d46d737e7a89795973f50c3d5a76f816
Manuel Aguiar <manuelmnaguiar00@gmail.com>
2024-12-06 11:09:25 +0000

    adding program MAIN FUNCTION

--- Webserv/main.cpp
+++ Webserv/main.cpp
@@ -3,20 +3,46 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mmaria-d <mmaria-d@student.42lisboa.com    +#+  +:+       +#+        */
+/*   By: manuel <manuel@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/18 10:33:07 by mmaria-d          #+#    #+#             */
-/*   Updated: 2024/11/18 10:40:29 by mmaria-d         ###   ########.fr       */
+/*   Updated: 2024/12/06 11:08:45 by manuel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+// Project headers
+# include "Globals/Globals.hpp"
+# include "ServerConfig/ServerConfig.hpp"
+# include "ServerConfig/DefaultConfig/DefaultConfig.hpp"
+# include "ServerManager/ServerManager.hpp"
 
+/*
+	WIP
 
+	Main function for the project
+	Estabablish pseudo-global variables
+	Load and parse the configuration file
+	Instantiate the ServerManager
+
+	RUN
+*/
 int main(int ac, char** av, char** env)
 {
-    (void)ac;
-    (void)av;
-    (void)env;
+    if (ac != 2)
+	{
+		std::cerr << "Usage: " << av[0] << " <config_file>" << std::endl;
+		return (1);
+	}
+
+	Clock			clock;
+	LogFile			statusFile("status.log");
+	LogFile			errorFile("error.log");
+	LogFile			debugFile("debug.log");
+	Globals			globals(&clock, &statusFile, &errorFile, &debugFile);
+	ServerConfig	config(av[1], DefaultConfig(), &globals);
+	ServerManager	server(config, &globals);
+
+	server.run();
 
     return (0);
 }
