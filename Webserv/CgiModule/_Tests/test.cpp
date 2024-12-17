953c24a78cb38d1a85c10898b6a0475713fe4ad8
Manuel Aguiar <113345121+manuel-aguiar@users.noreply.github.com>
2024-12-17 14:21:54 +0000

    added test folders to CgiModule

---
+++ Webserv/CgiModule/_Tests/test.cpp
@@ -0,0 +1,21 @@
+
+
+# include "../CgiModule.hpp"
+# include "TestDependencies.hpp"
+# include <iostream>
+
+int main(void)
+{
+
+	try
+	{
+		std::cout << "	PASS\n";
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << "	FAILED: " << e.what() << '\n';
+	}
+
+	return (0);
+}
+
