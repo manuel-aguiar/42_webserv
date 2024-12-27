9183601e0d60182100c6bda1a2ff69c11cd99313
Manuel Aguiar <113345121+manuel-aguiar@users.noreply.github.com>
2024-12-27 12:37:21 +0000

    Tookit: moving old to WIP, setting up tests on memoryPools

--- Toolkit/MemoryPool/MemoryPool.h
+++ Toolkit/MemoryPool/MemoryPool.h
@@ -6,7 +6,7 @@
 /*   By: mmaria-d <mmaria-d@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/25 12:20:05 by mmaria-d          #+#    #+#             */
-/*   Updated: 2024/12/27 10:20:04 by mmaria-d         ###   ########.fr       */
+/*   Updated: 2024/12/27 12:33:18 by mmaria-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,17 @@
 
 # define MEMORYPOOL_H
 
-# include "Alloc/SharedMPool_Alloc.hpp"
-# include "Dealloc/SharedMPool_Dealloc.hpp"
-# include "AlignDealloc/SharedMPool_AlignDealloc.hpp"
 # include "Nginx_MemoryPool/Nginx_MemoryPool.hpp"
 # include "Nginx_MemoryPool/Nginx_MPool_FixedElem/Nginx_MPool_FixedElem.hpp"
 # include "Nginx_MemoryPool/Nginx_MPool_FixedElem/Nginx_PoolAllocator_FixedElem.hpp"
 # include "Nginx_MemoryPool/Nginx_PoolAllocator.hpp"
 # include "MPool_FixedElem/MPool_FixedElem.hpp"
-# include "ContainerInPool/ContainerInPool.hpp"
+
+//Work in Progress
+
+//# include "Alloc/SharedMPool_Alloc.hpp"
+//# include "Dealloc/SharedMPool_Dealloc.hpp"
+//# include "AlignDealloc/SharedMPool_AlignDealloc.hpp"
+//# include "ContainerInPool/ContainerInPool.hpp"
 
 #endif
--- Toolkit/MemoryPool/Nginx_MemoryPool/Nginx_MPool_FixedElem/_Tests/run.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/bash
-clear
-c++ -Wall -Wextra -Werror -std=c++98 -g main.cpp ../../Nginx_MemoryPool.cpp ../../Nginx_MPool_Block.cpp -o a.out && valgrind ./a.out
-rm -rf vgcore*
-rm a.out
