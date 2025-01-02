d4725ac287fb07a42a05f9786a6e0aac9fb54323
Manuel Aguiar <manuelmnaguiar00@gmail.com>
2025-01-02 10:06:58 +0000

    small reorg ThreadPool tests

--- Toolkit/ThreadPool/_Tests/test1.cpp
+++ Toolkit/ThreadPool/_Tests/test1.cpp
@@ -56,15 +56,15 @@ static long fibGood(unsigned int n)
 class FiboTask : public IThreadTask
 {
 	public:
-		FiboTask() : m_n(0), m_result(NULL) {}
-		FiboTask(unsigned int n, long* result) : m_n(n), m_result(result) {}
+		FiboTask() : m_sequencePosition(0), m_placeResult(NULL) {}
+		FiboTask(unsigned int n, long* placeResult) : m_sequencePosition(n), m_placeResult(placeResult) {}
 		void execute()
 		{
-			*m_result = fibGood(m_n);
+			*m_placeResult = fibGood(m_sequencePosition);
 		}
 	private:
-		unsigned int 	m_n;
-		long* 			m_result;
+		unsigned int 	m_sequencePosition;
+		long* 			m_placeResult;
 };
 
 
@@ -118,8 +118,8 @@ int	TestPart1(int testNumber)
 		if (tp.getThreadCount() != 0)
 			throw std::runtime_error("Thread count is not 0");
 
-		long result;
-		FiboTask task(6, &result);
+		long placeResult;
+		FiboTask task(6, &placeResult);
 
 		tp.addTask(task);
 
@@ -128,7 +128,7 @@ int	TestPart1(int testNumber)
 		tp.waitForCompletion();
 		tp.removeThread();
 
-		if (result != 8)
+		if (placeResult != 8)
 			throw std::runtime_error("Didn't calculate fibonacci right");
 
 		std::cout << "	PASSED" << std::endl;
@@ -143,48 +143,6 @@ int	TestPart1(int testNumber)
 	return (testNumber);
 }
 
-int TestPart2(int testNumber)
-{
-/************************************************************** */
-	std::cout << "TEST " << testNumber << ": ";
-	try
-	{
-		const int numberOfTasks = 100;
-
-		ThreadPool 					tp(10, 20);
-		std::vector<long> 			fiboExpected;
-		std::vector<long>			fiboPlaceResult;
-		std::vector<FiboTask> 		tasks;
-
-		fiboExpected.reserve(numberOfTasks);
-		fiboPlaceResult.reserve(numberOfTasks);
-		tasks.reserve(numberOfTasks);
-
-		for (size_t i = 0; i < numberOfTasks; ++i)
-		{
-			fiboExpected.push_back(fibGood(i));
-			tasks.push_back(FiboTask(i, &fiboPlaceResult[i]));
-			tp.addTask(tasks[i]);
-		}
-			
-		tp.waitForCompletion();
-
-		for (size_t i = 0; i < fiboExpected.size(); ++i)
-		{
-			if (fiboExpected[i] != fiboPlaceResult[i])
-				throw std::runtime_error("Didn't calculate fibonacci right");
-		}
-
-		std::cout << "	PASSED" << std::endl;
-	}
-	catch(const std::exception& e)
-	{
-		std::cout << "	FAILED: " << e.what()  << std::endl;
-        TEST_FAIL_INFO();
-	}
-	testNumber++;	
-	return (testNumber);
-}
 
 
 
--- Toolkit/ThreadPool/_Tests/test2.cpp
+++ Toolkit/ThreadPool/_Tests/test2.cpp
@@ -1,30 +1,37 @@
 /* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   test2.cpp                                          :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: mmaria-d <mmaria-d@student.42lisboa.com    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/12/26 22:40:28 by mmaria-d          #+#    #+#             */
-/*   Updated: 2025/01/02 09:57:41 by mmaria-d         ###   ########.fr       */
-/*                                                                            */
+/*																			*/
+/*														:::	  ::::::::   */
+/*   testMain.cpp									   :+:	  :+:	:+:   */
+/*													+:+ +:+		 +:+	 */
+/*   By: mmaria-d <mmaria-d@student.42lisboa.com	+#+  +:+	   +#+		*/
+/*												+#+#+#+#+#+   +#+		   */
+/*   Created: 2024/08/27 08:33:11 by mmaria-d		  #+#	#+#			 */
+/*   Updated: 2024/09/03 16:26:42 by mmaria-d		 ###   ########.fr	   */
+/*																			*/
 /* ************************************************************************** */
 
 # include <iostream>
-# include "../ThreadPool/ThreadPool.hpp"
+# include "../ThreadPool.h"
 
 
 # include <unistd.h>
 # include <cstring>
 # include <sstream>
 # include <cstdlib>
+# include <vector>
 
-# include "../../MemoryPool/MemoryPool.h"
 # include "../../_Tests/ToolkitDummy.hpp"
 # include "../../_Tests/ToolkitBase.hpp"
 # include "../../_Tests/ToolkitDerived.hpp"
-# include "../../Arrays/StackArray/StackArray.hpp"
 # include "../../_Tests/test.h"
+
+template <typename T>
+std::string to_string(const T& value)
+{
+    std::ostringstream oss;
+    oss << value;
+    return oss.str();
+}
 
 static long fibGood(unsigned int n)
 {
@@ -46,128 +53,51 @@ static long fibGood(unsigned int n)
 	return (a);
 }
 
-
-template<typename T>
-struct State
+class FiboTask : public IThreadTask
 {
-    State() :  m_result(0), m_ready(false)
-    {
-        pthread_mutex_init(&m_mutex, NULL);
-        pthread_cond_init(&m_cond, NULL);
-    }
-    ~State()
-    {
-        pthread_mutex_destroy(&m_mutex);
-        pthread_cond_destroy(&m_cond);
-    }
-    T       get()
-    {
-        pthread_mutex_lock(&m_mutex);
-        while (!m_ready)
-            pthread_cond_wait(&m_cond, &m_mutex);
-        pthread_mutex_unlock(&m_mutex);
-        return (m_result);
-    }
-
-    void    set(const T& value) 
-    {
-        pthread_mutex_lock(&m_mutex);
-        m_result = value;
-        m_ready = true;
-        pthread_cond_signal(&m_cond);
-        pthread_mutex_unlock(&m_mutex);
-    }
-
-    pthread_mutex_t m_mutex;
-    pthread_cond_t  m_cond;
-    T               m_result;
-    bool            m_ready;
+	public:
+		FiboTask() : m_sequencePosition(0), m_placeResult(NULL) {}
+		FiboTask(unsigned int n, long* placeResult) : m_sequencePosition(n), m_placeResult(placeResult) {}
+		void execute()
+		{
+			*m_placeResult = fibGood(m_sequencePosition);
+		}
+	private:
+		unsigned int 	m_sequencePosition;
+		long* 			m_placeResult;
 };
 
-template <typename T>
-class Future
-{
-    public:
-        Future() {}
-        
-        Future(State<T>& m_state) : m_state(m_state) {}
-        ~Future() {}
-        Future(const Future& other) : m_state(other.m_state) {}
-        Future& operator=(const Future& other)
-        {
-            if (this != &other)
-                m_state = other.m_state;
-            return (*this);
-        }
-
-
-        long get()
-        {
-            return (m_state.get());
-        }
-
-    private:
-        State<T>&    m_state;
-};
-
-template <typename T>
-class Promise : public IThreadTask
-{
-    public:
-
-        Promise(size_t n = 0) : m_n(n), m_askedFuture(false), m_state()
-        {
-            
-        }
-        
-        void execute()
-        {
-            set_value(fibGood(m_n));
-        }
-        
-        void    set_value(const T& value)
-        {
-            m_state.set(value);
-        }
-
-        Future<T>   get_future()
-        {
-            if (m_askedFuture)
-                throw std::runtime_error("Future already asked");
-            m_askedFuture = true;
-            return (Future<T>(m_state));
-        }
-
-        
-    private:
-        size_t 	                m_n;
-        bool                    m_askedFuture;
-        State<T>                m_state;
-};
-
-int TestPart3(int testNumber)
+int TestPart2(int testNumber)
 {
 /************************************************************** */
 	std::cout << "TEST " << testNumber << ": ";
 	try
 	{
+		const int numberOfTasks = 100;
 
-		ThreadPool 					    tp(10, 20);
-		size_t 		                    fiboExpected;
+		ThreadPool 					tp(10, 20);
+		std::vector<long> 			fiboExpected;
+		std::vector<long>			fiboPlaceResult;
+		std::vector<FiboTask> 		tasks;
 
-		Promise<size_t>	                promise(50);
-        Future<size_t>                  future = promise.get_future();
-        
+		fiboExpected.reserve(numberOfTasks);
+		fiboPlaceResult.reserve(numberOfTasks);
+		tasks.reserve(numberOfTasks);
 
-        fiboExpected = fibGood(50);
-        tp.addTask(promise);
+		for (size_t i = 0; i < numberOfTasks; ++i)
+		{
+			fiboExpected.push_back(fibGood(i));
+			tasks.push_back(FiboTask(i, &fiboPlaceResult[i]));
+			tp.addTask(tasks[i]);
+		}
+			
+		tp.waitForCompletion();
 
-        size_t result = future.get();           // code blocks until this value is available
-
-        if (result != fiboExpected)
-            throw std::runtime_error("Didn't calculate fibonacci right");
-
-        tp.waitForCompletion();
+		for (size_t i = 0; i < fiboExpected.size(); ++i)
+		{
+			if (fiboExpected[i] != fiboPlaceResult[i])
+				throw std::runtime_error("Didn't calculate fibonacci right");
+		}
 
 		std::cout << "	PASSED" << std::endl;
 	}
@@ -179,3 +109,8 @@ int TestPart3(int testNumber)
 	testNumber++;	
 	return (testNumber);
 }
+
+
+
+
+
