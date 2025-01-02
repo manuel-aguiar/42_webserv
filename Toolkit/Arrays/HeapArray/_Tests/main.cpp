c8447554666f182195dcffc024413d4483694a28
Manuel Aguiar <manuelmnaguiar00@gmail.com>
2025-01-02 10:27:33 +0000

    Implementation of HeapArray

---
+++ Toolkit/Arrays/HeapArray/_Tests/main.cpp
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmaria-d <mmaria-d@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/08 08:40:54 by mmaria-d          #+#    #+#             */
+/*   Updated: 2025/01/02 10:24:02 by mmaria-d         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// C++ headers
+#include <iostream>
+#include <cstring>
+#include <string>
+#include <vector>
+
+// Project headers
+# include "../HeapArray.hpp"
+# include "../../../_Tests/ToolkitDummy.hpp"
+# include "../../../_Tests/ToolkitBase.hpp"
+# include "../../../_Tests/ToolkitDerived.hpp"
+
+# include "../../../_Tests/test.h"
+
+extern int TestPart1(int testNumber);
+extern int TestPart2(int testNumber);
+extern int TestPart3(int testNumber);
+
+int main()
+{
+    int testNumber = 1;
+    
+	std::cout << "\n*************** HeapArray tests ***************" << std::endl;
+	
+    testNumber = TestPart1(testNumber);
+    testNumber = TestPart2(testNumber);
+    testNumber = TestPart3(testNumber);
+
+	std::cout << "*********************************************\n" << std::endl;
+
+    return (0);
+}
+
+/*
+    c++ -Wall -Wextra -Werror -std=c++98 -g main.cpp ../../../MemoryPool/Nginx_MemoryPool/Nginx_MemoryPool.cpp ../../../MemoryPool/Nginx_MemoryPool/Nginx_MPool_Block.cpp -o main && ./main
+*/
