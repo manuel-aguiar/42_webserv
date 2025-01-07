f7f6f7dd1251ae076fca1b04910ad83db7fbfd56
Manuel Aguiar <113345121+manuel-aguiar@users.noreply.github.com>
2025-01-07 11:39:14 +0000

    Preparing Callback and Event for PR

--- Webserv/Event/Event.hpp
+++ Webserv/Event/Event.hpp
@@ -6,7 +6,7 @@
 /*   By: mmaria-d <mmaria-d@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/27 16:17:15 by mmaria-d          #+#    #+#             */
-/*   Updated: 2025/01/07 11:11:20 by mmaria-d         ###   ########.fr       */
+/*   Updated: 2025/01/07 11:38:46 by mmaria-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,6 @@
 
 # include "../GenericUtils/Webserver_Definitions.h"
 # include "../Callback/Callback.hpp"
-
-
 
 class Event
 {
