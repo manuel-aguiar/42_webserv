bf8d3c86da0cdc8065dad2f13cfb2e306b3bacb9
rajh-phuyal <rajhphuyal44@gmail.com>
2025-01-17 23:30:15 +0000

    keeps giving

--- Toolkit/_WIP/Arrays/HeapArray/HeapArray.hpp
+++ Toolkit/_WIP/Arrays/HeapArray/HeapArray.hpp
@@ -1,31 +1,6 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-<<<<<<<< HEAD:Webserv/HttpModule/HttpRequest/parsers/http_parse_message_body.cpp
-/*   http_parse_message_body.cpp                        :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: rphuyal <rphuyal@student.42lisboa.com>     +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/16 21:15:00 by rphuyal           #+#    #+#             */
-/*   Updated: 2025/01/16 21:38:18 by rphuyal          ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
-#include "../HttpRequest.hpp"
-
-
-int HttpRequest::mf_parseBody(const std::string& data)
-{
-    (void)data;
-    return RequestStatus::OK;
-}
-
-int HttpRequest::mf_parseChunkedBody(const std::string& data)
-{
-    (void)data;
-    return RequestStatus::OK;
-}
-========
 /*   HeapArray.hpp                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mmaria-d <mmaria-d@student.42lisboa.com    +#+  +:+       +#+        */
@@ -45,4 +20,3 @@ class HeapArray;
 # include "HeapArray.tpp"
 
 #endif
->>>>>>>> main:Toolkit/_WIP/Arrays/HeapArray/HeapArray.hpp
