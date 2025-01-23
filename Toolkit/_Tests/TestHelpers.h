8f4fa2b77e29499d4eacbd5bd3270dfeff1f4810
Manuel Aguiar <manuelmnaguiar00@gmail.com>
2025-01-23 16:14:32 +0000

    more conside testing

--- Toolkit/_Tests/TestHelpers.h
+++ Toolkit/_Tests/TestHelpers.h
@@ -21,6 +21,30 @@ namespace TestHelpers
 
 
 	std::string FileLineFunction(const char* file, const int line, const char* function);
+
+	template <typename T>
+	void assertEqual(T actual, T expected, const std::string& message, const char* file, int line, const char* function)
+	{
+		if (actual != expected)
+		{
+			throw std::logic_error(
+				message + " result was " + StringUtils::to_string(actual) +
+				" but expected: " + StringUtils::to_string(expected) + "\n" +
+				TestHelpers::FileLineFunction(file, line, function));
+		}
+	}
+
+	template <typename T>
+	void assertUnequal(T actual, T expected, const std::string& message, const char* file, int line, const char* function)
+	{
+		if (actual == expected)
+		{
+			throw std::logic_error(
+				message + " result was " + StringUtils::to_string(actual) +
+				" but expected: " + StringUtils::to_string(expected) + "\n" +
+				TestHelpers::FileLineFunction(file, line, function));
+		}
+	}
 }
 
 
