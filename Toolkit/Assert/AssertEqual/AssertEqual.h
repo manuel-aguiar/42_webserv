93dbb6edbe61282f6fab44bb0f9ceef7d2884068
Manuel Aguiar <manuelmnaguiar00@gmail.com>
2025-01-25 20:34:08 +0000

    some missing assert macro in Manager

--- Toolkit/Assert/AssertEqual/AssertEqual.h
+++ Toolkit/Assert/AssertEqual/AssertEqual.h
@@ -6,16 +6,27 @@
 		#include <iostream>
 		#include <cstdlib> // for abort
 		#include "../../TestHelpers/TestHelpers.h"
-		#define ASSERT_EQUAL(actual, expected, message)                                 \
-			do                                                                          \
-			{                                                                           \
-				if ((actual) != (expected))                                             \
-				{                                                                       \
-					std::cerr << "\n\n" << "\tABORTING PROGRAM\n"									\
-					<< TestHelpers::errorMsg((actual), (expected), (message), __FILE__, __LINE__, __FUNCTION__) << std::endl; \
-					std::abort();                                                       \
-				}                                                                       \
-			} while (0)
+		#define ASSERT_EQUAL(actual, expected, message)                                		 				\
+			do                                                                          					\
+			{                                                                           					\
+				if ((actual) != (expected))                                             					\
+				{                                                                       					\
+					std::cerr << "\n\n"                                               						\
+								<< "\tABORTING PROGRAM\n"                               					\
+								<< "\t----------------------------------\n"             					\
+								<< "\tError:     '" << (message) 	<< "'\n"             					\
+								<< "\tResult:    '" << (actual) 	<< "' [" << #actual 	<< "]\n"        \
+								<< "\tExpected:  '" << (expected) 	<< "' [" << #expected 	<< "]\n" 		\
+								<< "\t----------------------------------\n"             					\
+								<< "\tFile:       " << __FILE__ 	<< ":" << __LINE__ << "\n" 				\
+								<< "\tLine:       " << __LINE__ 	<< "\n"               					\
+								<< "\tFunction:   " << __FUNCTION__ << "\n"           						\
+								<< "\t----------------------------------\n"             					\
+								<< std::endl;                                          						\
+					std::abort();                                                     						\
+				}                                                                   						\
+      } while (0)
+
 	#else
 
 		#define ASSERT_EQUAL(actual, expected, message) ((void)0)
