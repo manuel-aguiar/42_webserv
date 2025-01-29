e293919251a1cc9652892f5beda3bd32850c9de7
Manuel Aguiar <manuelmnaguiar00@gmail.com>
2025-01-29 15:39:03 +0000

    some formatting

--- Webserv/Events/Subscription/SubscriptionGetSetAccess.cpp
+++ Webserv/Events/Subscription/SubscriptionGetSetAccess.cpp
@@ -6,40 +6,47 @@
 
 namespace Events
 {
-	Subscription::User				Subscription::accessUser()
+	Subscription::User
+	Subscription::accessUser()
 	{
 		return (m_user);
 	}
 
-	Subscription::Callback			Subscription::accessCallback()
+	Subscription::Callback
+	Subscription::accessCallback()
 	{
 		return (m_callback);
 	}
 
 	// getters
-	Ws::fd						Subscription::getFd() const
+	Ws::fd
+	Subscription::getFd() const
 	{
 		return (m_fdmask.getFd());
 	}
 
-	Events::Monitor::Mask			Subscription::getMonitoredEvents() const
+	Events::Monitor::Mask
+	Subscription::getMonitoredEvents() const
 	{
 		return (m_monitoredEvents);
 	}
 
-	Events::Monitor::Mask			Subscription::getTriggeredEvents() const
+	Events::Monitor::Mask
+	Subscription::getTriggeredEvents() const
 	{
 		return (m_triggeredEvents);
 	}
 
 	// setters
 
-	void	Subscription::setFd(const Ws::fd fd)
+	void
+	Subscription::setFd(const Ws::fd fd)
 	{
 		m_fdmask.setFd(fd);
 	}
 
-	void	Subscription::setMonitoredEvents(const Events::Monitor::Mask flags)
+	void
+	Subscription::setMonitoredEvents(const Events::Monitor::Mask flags)
 	{
 		//solved at compile time :)
 		const int validFlags = 0 	|	Events::Monitor::NONE
@@ -55,17 +62,20 @@ namespace Events
 		m_monitoredEvents = flags;
 	}
 
-	void	Subscription::setUser(const Subscription::User user)
+	void
+	Subscription::setUser(const Subscription::User user)
 	{
 		m_user = user;
 	}
 
-	void	Subscription::setCallback(const Subscription::Callback handler)
+	void
+	Subscription::setCallback(const Subscription::Callback handler)
 	{
 		m_callback = handler;
 	}
 
-	bool	Subscription::isSubscribed() const
+	bool
+	Subscription::isSubscribed() const
 	{
 		return (m_fdmask.getState() == Subscription::SUBSCRIBED);
 	}
