b47bb941a56f430265a0d8964b5797f4c908ff91
rafaelva-z <rafaelvaz.art@gmail.com>
2025-01-30 17:50:12 +0000

    Implemented http header parsing (HttpRequest::mf_parseHeaders)

--- Webserv/HttpModule/HttpRequest/parsers/http_parse_header_line.cpp
+++ Webserv/HttpModule/HttpRequest/parsers/http_parse_header_line.cpp
@@ -7,9 +7,27 @@
 /* *********************************/
 
 #include "../HttpRequest.hpp"
+#include <sstream>
 
 int HttpRequest::mf_parseHeaders(const std::string& data)
 {
-    (void)data;
-    return Http::Status::OK;
+	std::istringstream	stringStream(data);
+	std::string			line;
+
+	while (std::getline(stringStream, line) && !line.empty())
+	{
+		if (line[line.size() - 1] == '\r')
+			line.erase(line.size() - 1);
+		std::size_t colonPos = line.find(':');
+		if (colonPos == std::string::npos)
+			return (Http::Status::BAD_REQUEST);
+		std::string key = line.substr(0, colonPos);
+		std::string value = line.substr(colonPos + 1);
+
+		while (!value.empty() && (value[0] == ' ' || value[0] == '\t')) {
+			value.erase(0, 1);
+		}
+		m_headers[key] = value;
+	}
+	return (Http::Status::OK);
 }
