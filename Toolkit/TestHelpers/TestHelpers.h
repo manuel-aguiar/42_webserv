501b1f120c95e34e82c023b34aa68265523f4a0d
Manuel Aguiar <113345121+manuel-aguiar@users.noreply.github.com>
2025-01-30 08:48:24 +0000

    expect equal macro!

--- Toolkit/TestHelpers/TestHelpers.h
+++ Toolkit/TestHelpers/TestHelpers.h
@@ -46,6 +46,26 @@ namespace TestHelpers
 
 }
 
-
+	#ifndef EXPECT_EQUAL
+		#include <sstream>
+		#define EXPECT_EQUAL(actual, expected, message)                                \
+		do {                                                                           \
+			if ((actual) != (expected)) {                                              \
+				std::ostringstream oss;                                                \
+				oss << "\n\n"                                                          \
+					<< "\tEXPECT_EQUAL failed\n"                                       \
+					<< "\t----------------------------------\n"                        \
+					<< "\tError:     '" << (message)  << "'\n"                         \
+					<< "\tResult:    '" << (actual)   << "' [" << #actual   << "]\n"   \
+					<< "\tExpected:  '" << (expected) << "' [" << #expected << "]\n"   \
+					<< "\t----------------------------------\n"                        \
+					<< "\tFile:       " << __FILE__  << ":" << __LINE__ << "\n"        \
+					<< "\tLine:       " << __LINE__  << "\n"                           \
+					<< "\tFunction:   " << __FUNCTION__ << "\n"                        \
+					<< "\t----------------------------------\n";                        \
+				throw std::logic_error(oss.str());                                     \
+			}                                                                          \
+		} while (0)
+	#endif
 
 #endif
