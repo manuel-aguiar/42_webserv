b413423763904ce74c319ddb4979dd934b02a664
Manuel Aguiar <113345121+manuel-aguiar@users.noreply.github.com>
2025-02-03 09:45:26 +0000

    adding Globals refurbished

--- Webserv/Globals/Clock/_Tests/test.cpp
+++ Webserv/Globals/Clock/_Tests/test.cpp
@@ -1,11 +1,13 @@
 
 
 # include "../Clock.hpp"
-# include "TestDependencies.hpp"
+# include "../../../../Toolkit/TestHelpers/TestHelpers.h"
 
 int main(void)
 {
+	TEST_HEADER("Globals - Clock");
 
+	TEST_FOOTER;
 	return (0);
 }
 
