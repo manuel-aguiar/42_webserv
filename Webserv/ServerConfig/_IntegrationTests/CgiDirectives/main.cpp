d7c6e59c1078f00c390abf40e6fedaf469e9d4b5
Manuel Aguiar <manuelmnaguiar00@gmail.com>
2025-02-13 11:19:01 +0000

    fixing bugs ServerConfig::mf_expandCgiToLocations()

--- Webserv/ServerConfig/_IntegrationTests/CgiDirectives/main.cpp
+++ Webserv/ServerConfig/_IntegrationTests/CgiDirectives/main.cpp
@@ -159,6 +159,44 @@ int main(void)
 		TEST_FAILED_MSG(configPath + ": " + e.what());
 	}
 
+////////////////////////////////////////////////////////////////////////
+    configPath = "LocationOverride.conf";
+    try
+    {
+        TEST_INTRO(testNumber++);
+        ServerConfig config(configPath.c_str(), defaultConfig);
+
+        EXPECT_EQUAL(config.parseConfigFile(), true, "Should parse without issues");
+
+        EXPECT_EQUAL(config.getMaxConnections(), 100, "Wrong max connections");
+
+        const Config::CgiInterpreterMap& 
+        cgiInterpreters = config.getCgiInterpreters();
+
+        EXPECT_EQUAL(cgiInterpreters.size(), 2, "Wrong number of cgi interpreters");
+        EXPECT_EQUAL(cgiInterpreters.begin()->first, ".cgi", "wrong match");
+        EXPECT_EQUAL(cgiInterpreters.begin()->second, "/potato", "wrong match");
+
+        EXPECT_EQUAL(cgiInterpreters.rbegin()->first, ".py", "wrong match");
+        EXPECT_EQUAL(cgiInterpreters.rbegin()->second, "/usr/bin/python3", "wrong match");
+
+        EXPECT_EQUAL(config.getServerBlocks().size(), 1, "Wrong number of server blocks");
+        EXPECT_EQUAL(config.getServerBlocks()[0].getLocations().size(), 1, "Wrong number of locations");
+        
+        const Config::CgiInterpreterMap& 
+        locationInterpreters = config.getServerBlocks()[0].getLocations()[0].getCgiInterpreters();
+
+        EXPECT_EQUAL(locationInterpreters.size(), 1, "Wrong number of cgi interpreters");
+        EXPECT_EQUAL(locationInterpreters.begin()->first, ".py", "wrong match");
+        EXPECT_EQUAL(locationInterpreters.begin()->second, "tretas", "wrong match");
+
+        TEST_PASSED_MSG(configPath + ": " + "Location cgi, resolving to global");
+	}
+	catch (const std::exception& e)
+	{
+		TEST_FAILED_MSG(configPath + ": " + e.what());
+	}
+
     return (0);
 }
 
