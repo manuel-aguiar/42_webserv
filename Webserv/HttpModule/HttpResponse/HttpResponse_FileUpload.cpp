26b48fe703771cce232ec788c6cb276fdba1f901
Manuel Aguiar <manuelmnaguiar00@gmail.com>
2025-02-27 17:35:33 +0000

    merge main

--- Webserv/HttpModule/HttpResponse/HttpResponse_FileUpload.cpp
+++ Webserv/HttpModule/HttpResponse/HttpResponse_FileUpload.cpp
@@ -31,30 +31,32 @@ namespace Http
 		|| m_responseData.requestData->multipart_Filename.empty())
 		{
 			m_file.close();
+
+			// CHECK FOR FAILURE IN EXECUTION NEEDED, UNLINK FILE
+			
 			return (view);
 		}
 
 		if (m_responseData.requestData->multipart_Filename != m_file.path())
 		{
 			m_file.close();
-			if (!m_file.open(m_responseData.requestData->multipart_Filename.c_str()))
-			{
-				m_responseData.requestStatus = Http::Status::INTERNAL_ERROR;
-				m_processFunction = &Response::mf_processBodyNone;
-				m_fillFunction = &Response::mf_fillErrorResponse;
-				return (view);
-			}
+			if (!m_file.open(m_responseData.requestData->multipart_Filename.c_str(),
+				O_CREAT | O_RDWR | O_TRUNC, 0666))
+				goto exitFailure;
 		}
-		int bytesWritten = m_file.write(view.data(), view.size());
+
+		bytesWritten = m_file.write(view.data(), view.size());
 		if (bytesWritten < 0)
-		{
-			m_responseData.requestStatus = Http::Status::INTERNAL_ERROR;
-			m_processFunction = &Response::mf_processBodyNone;
-			m_fillFunction = &Response::mf_fillErrorResponse;
-			return (view);
-		}
-		BufferView remaining = view.substr(bytesWritten, view.size() - bytesWritten);
+			goto exitFailure;
+		
+		remaining = view.substr(bytesWritten, view.size() - bytesWritten);
 
 		return (remaining);
+	
+	exitFailure:
+		m_responseData.requestStatus = Http::Status::INTERNAL_ERROR;
+		m_processFunction = &Response::mf_processBodyNone;
+		m_fillFunction = &Response::mf_fillErrorResponse;
+		return (view);
 	}
 }
