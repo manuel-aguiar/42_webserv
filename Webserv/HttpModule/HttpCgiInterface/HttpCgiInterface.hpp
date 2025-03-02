98e2cb169af7f9a297f3e4f61ea784f62820e08a
Manuel Aguiar <manuelmnaguiar00@gmail.com>
2025-03-02 11:34:52 +0000

    cleanup

--- Webserv/HttpModule/HttpCgiInterface/HttpCgiInterface.hpp
+++ Webserv/HttpModule/HttpCgiInterface/HttpCgiInterface.hpp
@@ -15,26 +15,26 @@ namespace Cgi { class Module;}
 
 namespace Http
 {
-    class CgiInterface
-    {
-        public:
-            CgiInterface(Cgi::Module& cgiModule, const size_t numberOfResponses);
-            ~CgiInterface();
+	class CgiInterface
+	{
+		public:
+			CgiInterface(Cgi::Module& cgiModule, const size_t numberOfResponses);
+			~CgiInterface();
 
-            Http::CgiResponse*   acquireGateway();
-            void                releaseGateway(Http::CgiResponse& gateway);
+			Http::CgiResponse*   acquireGateway();
+			void                releaseGateway(Http::CgiResponse& gateway);
 
-        private:
-            Cgi::Module&                            m_cgiModule;
-            HeapArray<Http::CgiResponse>             m_cgiGateways;
-            HeapCircularQueue<Http::CgiResponse*>    m_availableGateways;
+		private:
+			Cgi::Module&                                m_cgiModule;
+			HeapArray<Http::CgiResponse>                m_cgiGateways;
+			HeapCircularQueue<Http::CgiResponse*>       m_availableGateways;
 
-            CgiInterface(const CgiInterface& other);
-            CgiInterface& operator=(const CgiInterface& other);
-    };
+			CgiInterface(const CgiInterface& other);
+			CgiInterface& operator=(const CgiInterface& other);
+	};
 
 
-    
+	
 }
 
 
