f81eb7e1a32f307184bfa69a2041b838832ae7ef
Manuel Aguiar <manuelmnaguiar00@gmail.com>
2025-03-07 21:30:01 +0000

    reordered Webserv Run

--- Webserv/Run/WebservRun.cpp
+++ Webserv/Run/WebservRun.cpp
@@ -115,19 +115,20 @@ int SingleServerRun(const char* programName, ServerConfig& config, Globals& glob
 
 		// preparing ServerContext;
 		ServerContext	context;
+		context.setServerConfig(config);
+
 		BlockFinder		blockFinder(blockFinderEntryCount(config));
+		context.setBlockFinder(blockFinder);
 
 		Cgi::Module		cgiModule(config.getMaxConcurrentCgi(), config.getMaxCgiBacklog(), 5000, 4096, eventManager, globals);
 		context.setAddonLayer(Ws::AddonLayer::CGI, &cgiModule);		
 		
 		Http::Module	httpModule(config.getMaxConnections(), context);
+		context.setAppLayer(Ws::AppLayer::HTTP, &httpModule, &Http::Module::InitConnection);
 
 		blockFinder.loadServerBlocks(config.getServerBlocks());
 
-		context.setAppLayer(Ws::AppLayer::HTTP, &httpModule, &Http::Module::InitConnection);
-		context.setBlockFinder(blockFinder);
 		context.setGlobals(globals);
-		context.setServerConfig(config);
 
 		// preparing server launch
 		Conn::Manager	connManager(config.getMaxConnections(), config.getAllBindAddresses(), eventManager, globals, context);
