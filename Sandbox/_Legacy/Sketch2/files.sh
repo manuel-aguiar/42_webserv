0ef8c8d6e020f536b04f294e9623ee07c4366f6c
rafaelva-z <rafaelvaz.art@gmail.com>
2025-03-08 13:37:33 +0000

    deleted lots of files

--- Sandbox/_Legacy/Sketch2/files.sh
+++
@@ -1,8 +0,0 @@
-for dir in */; do
-    # Check if it's a directory
-    if [ -d "$dir" ]; then
-        # Create .cpp and .hpp files in each subfolder
-        touch "${dir}${dir%/}.cpp"
-        touch "${dir}${dir%/}.hpp"
-    fi
-done
--- gitpush.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ $# -eq 0 ]; then
-	git add . && git commit -m "generic commit" && git push
-else
-	git add . && git commit -m "$1"	&& git push
-fi
-
-
