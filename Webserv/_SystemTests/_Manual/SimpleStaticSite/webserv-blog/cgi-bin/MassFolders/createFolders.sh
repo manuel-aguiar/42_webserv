b14633a2ce956efeb12a092b3eef1e9331c3515b
Raj Phuyal <rphuyal@c2r6s3.42lisboa.com>
2025-03-08 15:21:30 +0000

    change script and del yolo

--- Webserv/_SystemTests/_Manual/SimpleStaticSite/webserv-blog/cgi-bin/MassFolders/createFolders.sh
+++ Webserv/_SystemTests/_Manual/SimpleStaticSite/webserv-blog/cgi-bin/MassFolders/createFolders.sh
@@ -2,12 +2,14 @@
 
 # Define the parent directory (optional)
 PARENT_DIR="folders"
-mkdir -p "$PARENT_DIR"
+
+SCRIPT_DIR = "$(cd "$(dirname "$0")" && pwd)"
+mkdir -p "$SCRIPT_DIR/$PARENT_DIR"
 
 # Create 10,000 directories
 for i in $(seq -w 1 10000); do
-    mkdir -p "$PARENT_DIR/folder_$i"
+    mkdir -p "$SCRIPT_DIR/$PARENT_DIR/folder_$i"
 done
 
-echo "10,000 folders created in $PARENT_DIR."
+echo "10,000 folders created in $SCRIPT_DIR/$PARENT_DIR."
 
